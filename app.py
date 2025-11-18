87123f736b115ed99234e39c8298e935962a5f0a
Mirai385 <mirai1069@icloud.com>
2025-11-18 12:17:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ app_ui = ui.page_sidebar(
     ui.sidebar(
         
         ui.input_select(
-        "val", "項目を選択", choices=["投票率", "有権者数", "定数比", "有権者数（男女別）"], selected=None
+        "val", "1つ目の項目を選択:", choices=["投票率", "有権者数", "定数比", "有権者数（男女別）"], selected=None
         ),
         ui.input_select(
-        "val", "項目を選択", choices=["投票率", "有権者数", "定数比", "有権者数（男女別）"], selected=None
+        "val", "2つ目の項目を選択:", choices=["投票率", "有権者数", "定数比", "有権者数（男女別）"], selected=None
         ),
     ),    
     ui.output_plot("histgram"),
